fe15e2e42e4c8c07de07cd20f52542e50aca6883
Soumitra Ghosh <vegetassb34@gmail.com>
2023-05-09 17:32:43 +0530

    Update Mongoose
    Update

--- conifg/mongoose.js
+++ conifg/mongoose.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
 
-async function main() {
-    console.log('success')
-  await mongoose.connect('mongodb://127.0.0.1:27017/issue-tracker-db');
-  
 
-}
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  "mongodb+srv://soumitra:56789123%40Sg@cluster0.qtleogu.mongodb.net/issue-tracker?retryWrites=true&w=majority"
+);
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+//checking the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+//up and running then print the statement
+db.once("open", function () {
+  console.log("succesfully connected to database");
+});
+
+// exporting the db
+module.exports = db;
